0f0f54ba8ef30169ba4724d46bd1dfba8c3bb592
ManikumarGajam <manikumargajam@gmail.com>
2025-10-28 18:49:19 +0530

    about

--- src/components/Home.js
+++ src/components/Home.js
@@ -256,7 +256,7 @@ const Section = styled.section`
     padding-bottom: 10px;
   }
     @media (max-width: 1023px) {
-  min-height: 40vh !important;
+  min-height: 50vh !important;
   align-items: center !important;
   
   
