ebbfa4741c1a7e411735e37c032817fc06da5222
ManikumarGajam <manikumargajam@gmail.com>
2025-10-28 13:34:03 +0530

    Updated project files

--- src/components/Home.js
+++ src/components/Home.js
@@ -256,11 +256,10 @@ const Section = styled.section`
     padding-bottom: 10px;
   }
     @media (max-width: 1023px) {
-  min-height: 50vh !important;
-  height: 50vh !important;
+  min-height: 100vh !important;
+  
   align-items: center !important;
-  padding-top: 18px !important;
-  padding-bottom: 0 !important;
+  
 }
 
 `;
